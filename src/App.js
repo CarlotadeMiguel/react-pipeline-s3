8becdba1cd4151946f6e2b330d34f4cd09202435
CarlotadeMiguel <carlota.demiguel@tajamar365.com>
2025-07-22 18:10:27 +0200

    Aprende más sobre AWS S3

--- src/App.js
+++ src/App.js
@@ -3,8 +3,16 @@ function App() {
   return (
     <div className="App">
       <main className="App-main">
-        <h1 className='App-title' >Bienvenido a React App</h1>
-        <p className='App-text'>App desplegada en AWS S3</p>
+        <h1 className='App-title'>¡Bienvenido a mi App en React!</h1>
+        <p className='App-text'>Esta aplicación está desplegada en <strong>AWS S3</strong>.</p>
+        <a
+          className="App-button"
+          href="https://aws.amazon.com/s3/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Aprende más sobre AWS S3
+        </a>
       </main>
     </div>
   );
